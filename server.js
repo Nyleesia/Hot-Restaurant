5d938a0fa5a1221b98b38c30ed1e0da2cfebd400
Steven Dixon <stevendixon112@gmail.com>
2019-08-27 20:00:20 -0400

    added post route and start to package.json

--- server.js
+++ server.js
@@ -2,11 +2,12 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
+
 var tablelist = [];
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3001;
+var PORT = process.env.PORT || 3001;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -16,46 +17,56 @@ app.use(express.json());
 // =============================================================
 
 // Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "home.html"));
 });
 //displays tables and waiting list
-app.get("/tables", function(req, res) {
+app.get("/tables", function (req, res) {
   res.sendFile(path.join(__dirname, "tables.html"));
 });
 
 // Displays reservation addition
-app.get("/reserve", function(req, res) {
+app.get("/reserve", function (req, res) {
   res.sendFile(path.join(__dirname, "reservations.html"));
 });
 
 // Displays a table list
-app.get("/api/tables", function(req, res) {
-  
-  console.log(tablelist);
+app.get("/api/tables", function (req, res) {
 
+  console.log(tablelist);
+/*
   for (var i = 0; i < 5; i++) {
     var customerName = tablelist[i].customerName;
     var phoneNumber = tablelist[i].phoneNumber;
     var customerEmail = tablelist[i].customerEmail;
     var customerID = tablelist[i].customerID;
-    
-    $().append("Customer Name: "+customerName);
-    $().append("Phone Number: "+phoneNumber);
-    $().append("Customer Email: "+customerEmail);
-    $().append("Cusotmer Id: "+customerID);
+
+    $().append("Customer Name: " + customerName);
+    $().append("Phone Number: " + phoneNumber);
+    $().append("Customer Email: " + customerEmail);
+    $().append("Customer Id: " + customerID);
   }
-
-  
-});
-
-// Create New Characters - takes in JSON input
-app.post("/api/reseve", function(req, res) {
+*/
 
 });
+
+// Create a reserve post route
+app.post("/api/reserve", function (req, res) {
+  tablelist.push(
+    {
+      customerName: req.body.customerName || "",
+      phoneNumber: req.body.phoneNumber || "",
+      customerEmail: req.body.customerEmail || "",
+      customerID: req.body.customerID || ""
+    }
+  );
+  res.status(200).send("part added success");
+});
+
+
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
